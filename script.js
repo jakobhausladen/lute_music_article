1c20b40440f3ce582de12a2527557e79f6c7d319
Jakob Hausladen <jakob.hausladen@gmail.com>
2023-11-11 15:42:10 +0100

    sticky buttons

--- script.js
+++ script.js
@@ -1,40 +1,73 @@
-const buttons = document.querySelectorAll(".playPauseButton");
-const audioElements = document.querySelectorAll(".music");
-const playIcons = document.querySelectorAll(".playIcon");
-const pauseIcons = document.querySelectorAll(".pauseIcon");
+document.addEventListener("DOMContentLoaded", function () {
 
-buttons.forEach((button, index) => {
-    const audio = audioElements[index];
-    let isPlaying = false;
-    let audioPausedTime = 0;
 
-    // preload the GIFs
-    new Image().src = playIcons[index].src;
-    new Image().src = pauseIcons[index].src;
+    // GIFs and music
 
-    button.addEventListener("click", function() {
-        const audioFile = button.getAttribute("data-music");
+    const buttons = document.querySelectorAll(".playPauseButton");
+    const audioElements = document.querySelectorAll(".music");
+    const playIcons = document.querySelectorAll(".playIcon");
+    const pauseIcons = document.querySelectorAll(".pauseIcon");
 
-        if (isPlaying) {
-            audio.pause();
-            audioPausedTime = audio.currentTime;
-            playIcons[index].style.display = "inline";
-            pauseIcons[index].style.display = "none";
-        } else {
-            audio.src = audioFile;
-            audio.currentTime = audioPausedTime;
-            audio.play();
-            playIcons[index].style.display = "none";
-            pauseIcons[index].style.display = "inline";
-        }
+    buttons.forEach((button, index) => {
+        const audio = audioElements[index];
+        let isPlaying = false;
+        let audioPausedTime = 0;
 
-        isPlaying = !isPlaying;
+        // preload GIFs
+        new Image().src = playIcons[index].src;
+        new Image().src = pauseIcons[index].src;
 
-        audio.addEventListener("ended", function() {
-            playIcons[index].style.display = "inline";
-            pauseIcons[index].style.display = "none";
-            isPlaying = false;
-            audioPausedTime = 0;
+        button.addEventListener("click", function() {
+            const audioFile = button.getAttribute("data-music");
+
+            if (isPlaying) {
+                audio.pause();
+                audioPausedTime = audio.currentTime;
+                playIcons[index].style.display = "inline";
+                pauseIcons[index].style.display = "none";
+            } else {
+                audio.src = audioFile;
+                audio.currentTime = audioPausedTime;
+                audio.play();
+                playIcons[index].style.display = "none";
+                pauseIcons[index].style.display = "inline";
+            }
+
+            isPlaying = !isPlaying;
+
+            audio.addEventListener("ended", function() {
+                playIcons[index].style.display = "inline";
+                pauseIcons[index].style.display = "none";
+                isPlaying = false;
+                audioPausedTime = 0;
+            });
         });
+    });
+
+
+
+    // sticky mechanism
+
+    var elements = document.querySelectorAll('.button-container');
+    var stickyElement = document.getElementById('button_1');
+    var offsetMargin = 750;
+
+    var offsets = Array.from(elements).map(function (element) {
+      return element.offsetTop;
+    });
+
+    window.addEventListener('scroll', function () {
+      var scrollPosition = window.scrollY;
+
+      var currentElementIndex = offsets.findIndex(function (offset, index) {
+        var nextOffset = offsets[index + 1] || Infinity;
+        return scrollPosition + offsetMargin >= offset && scrollPosition + offsetMargin < nextOffset;
+      });
+
+      if (currentElementIndex >= 0) {
+        stickyElement.classList.remove('sticky');
+        stickyElement = elements[currentElementIndex];
+        stickyElement.classList.add('sticky');
+      }
     });
 });
